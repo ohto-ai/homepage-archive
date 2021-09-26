c347ac51a107ccb57ecb07a116d492eb7dee741c
OhtoAi <zhuguangfeng.thatboy@gmail.com>
2021-09-26 17:30:07 +0800

    add vs version image_proxy code

---
+++ dev/homepage-api-test/homepage-api-test/ImageProxy.hpp
@@ -0,0 +1,327 @@
+﻿#pragma once
+
+#ifndef _IMAGE_PROXY_HPP_
+#define _IMAGE_PROXY_HPP_
+
+#define OHTOAI_USE_BOOST
+
+#include "ohtoai_base.hpp"
+#include <iterator>
+#include <fstream>
+#include <set>
+#include <algorithm>
+#include <httplib.h>
+
+namespace ohtoai
+{
+	struct ImageFileInfo;
+
+
+	class ImageProxy
+	{
+		constexpr static auto SettingConfigPath{ "image_proxy.config" };
+		inline const static log::Log Logger{ "image_proxy.log" };
+
+#if defined WIN32 || defined _WIN32
+		std::string fileStorageBase{ R"(storage/)" };
+		std::string fileUrlBase{ R"(//localhost/img/)" };
+#else
+		std::string fileStorageBase{ R"(/home/ohtoai/html/assets/img/storage/)" };
+		std::string fileUrlBase{ R"(//thatboy.info/img/)" };
+#endif
+		std::string assemblyPath{ "assembly.dat" };
+
+
+		OHTOAI_DEFINE_TYPE_INTRUSIVE(ImageProxy, fileStorageBase, fileUrlBase, assemblyPath);
+	public:
+		OHTOAI_DEFINE_TYPE_GETTER_INTRUSIVE(fileUrlBase, FileUrlBase);
+		OHTOAI_DEFINE_TYPE_GETTER_INTRUSIVE(fileStorageBase, FileStorageBase);
+		OHTOAI_DEFINE_TYPE_GETTER_SETTER_INTRUSIVE(assemblyPath, AssemblyPath);
+
+		friend struct ImageFileInfo;
+	private:
+		ImageProxy(const ImageProxy&) = default;
+		ImageProxy& operator = (const ImageProxy&) = default;
+
+		ImageProxy()
+		{
+			::srand(static_cast<unsigned int>(::time(nullptr)));
+
+			loadConfig();
+		}
+
+		const std::string mergeImageStorage(std::string storage)
+		{
+			return getFileStorageBase() + storage;
+		}
+
+		const std::string mergeImageUrl(std::string storage)
+		{
+			return getFileUrlBase() + storage;
+		}
+
+	public:
+		static ImageProxy& instance();
+	public:
+		std::vector<const ImageFileInfo*> fetchImageSet(std::set<std::string> authors = {}, std::set<std::string> tags = {}) const;
+
+		std::string viewImageHtmlPage(std::string fileUrl)const;
+
+		void loadConfig();
+		void saveConfig();
+		void loadData();
+		void saveData();
+		void syncWithFile();
+
+		std::string viewImageHtmlPage(const ImageFileInfo& info)const;
+
+		ImageFileInfo createImageFile() const;
+
+		const ImageFileInfo* storageImage(ImageFileInfo&& info, std::string&& content);
+
+		void removeImage(std::vector<const ImageFileInfo*> infos);
+
+		const ImageFileInfo* fetchImage(std::string fileName);
+
+		virtual ~ImageProxy();
+	protected:
+		std::vector<ImageFileInfo> imageFileInfoList;
+	};
+	
+	struct ImageFileInfo
+	{
+	protected:
+		std::string uid{ "00000000-0000-0000-0000-000000000000" };	// uid
+		std::string name{ "null" };         // origin name
+		std::string time{ "null" };			// time
+		std::string author{ "null" };		// author
+		std::string type{ "png" };			// type
+		size_t size{};							// file size
+		int width{};							// image width
+		int height{};							// image height
+		std::set<std::string> tags;				// tags
+
+		mutable std::string storage{};			// [generated not save] storage file name
+		mutable std::string url{};				// [generated] url
+	public:
+		OHTOAI_DEFINE_TYPE_REFER_GETTER_SETTER_INTRUSIVE(uid, UID);
+		OHTOAI_DEFINE_TYPE_REFER_GETTER_SETTER_INTRUSIVE(name, Name);
+		OHTOAI_DEFINE_TYPE_REFER_GETTER_SETTER_INTRUSIVE(time, Time);
+		OHTOAI_DEFINE_TYPE_REFER_GETTER_SETTER_INTRUSIVE(author, Author);
+		OHTOAI_DEFINE_TYPE_REFER_GETTER_SETTER_INTRUSIVE(width, Width);
+		OHTOAI_DEFINE_TYPE_REFER_GETTER_SETTER_INTRUSIVE(height, Height);
+		OHTOAI_DEFINE_TYPE_REFER_GETTER_SETTER_INTRUSIVE(type, Type);
+		OHTOAI_DEFINE_TYPE_REFER_GETTER_SETTER_INTRUSIVE(size, Size);
+		OHTOAI_DEFINE_TYPE_REFER_GETTER_SETTER_INTRUSIVE(tags, Tags);
+
+		std::string getStorage() const
+		{
+			if (storage.empty()&& !uid.empty() && !type.empty())
+			{
+				storage = uid + '.' + type;
+			}
+			return storage;
+		}
+
+		void completeInfo()
+		{
+			setTime(time::getFormatedServerTime(0, false));
+			storage = uid + '.' + type;
+			url = ImageProxy::instance().mergeImageUrl(getStorage());			
+		}
+		std::string getUrl() const
+		{
+			if (url.empty()&& !getStorage().empty())
+			{
+				url = ImageProxy::instance().mergeImageUrl(getStorage());
+			}
+			return url;
+		}
+		
+		OHTOAI_DEFINE_TYPE_INTRUSIVE(ImageFileInfo, uid, name, time, author, width, height, type, size, tags, url);
+	};
+	
+	inline std::vector<const ImageFileInfo*> ImageProxy::fetchImageSet(std::set<std::string> authors, std::set<std::string> tags) const
+	{
+		std::vector<const ImageFileInfo*> limitedFileInfos;
+		for (const auto& info : imageFileInfoList)
+		{
+			bool ifAllLimitMatched = true;
+
+			if (!authors.empty() && authors.find(info.getAuthor()) == authors.end())
+				continue;
+
+			if (!tags.empty() && !std::includes(info.getTags().begin(), info.getTags().end(), tags.begin(), tags.end()))
+				continue;
+
+			// todo
+			limitedFileInfos.push_back(&info);
+		}
+		return limitedFileInfos;
+	}
+
+	inline std::string ImageProxy::viewImageHtmlPage(std::string fileUrl) const
+	{
+		return std::string{ R"(<!DOCTYPE html><html lang="en"><style>*{border:0;margin:0;width:100%;height:100%}*::-webkit-scrollbar{display:none}</style><iframe src=")" } + fileUrl + R"("></iframe></html>")";
+	}
+
+	inline void ImageProxy::loadConfig() {
+		std::ifstream ifs(SettingConfigPath);
+		if (!ifs)
+		{
+			LOG_INFO("Config missing.");
+			saveConfig();
+		}
+		else
+		{
+			std::istreambuf_iterator<char> beg(ifs), end;
+			from_json(nlohmann::json::parse(std::string(beg, end)), *this);
+			LOG_INFO("Config readed.");
+		}
+
+		LOG_INFO("File storage at ", getFileStorageBase());
+		LOG_INFO("Assembly at ", getAssemblyPath());
+		LOG_INFO("File url at ", getFileUrlBase());
+		if (!file::access(getFileStorageBase()))
+		{
+			file::createDirectoryRecursively(getFileStorageBase());
+			LOG_INFO("Create folder ", getFileStorageBase());
+		}
+		loadData();
+	}
+
+	inline void ImageProxy::saveConfig()
+	{
+		std::thread{
+			[=] {
+					nlohmann::json j = *this;
+					auto storageData{ j.dump() };
+					std::ofstream ofs(SettingConfigPath);
+					ofs.write(storageData.data(), storageData.size());
+					ofs.close();
+					LOG_INFO("Config saved.");
+				}
+		}.detach();
+	}
+
+	inline void ImageProxy::loadData()
+	{
+		std::ifstream ifs(getAssemblyPath());
+		if (!ifs)
+		{
+			LOG_INFO("Assembly missing.");
+			saveData();
+		}
+		else
+		{
+			std::istreambuf_iterator<char> beg(ifs), end;
+			nlohmann::json::parse(std::string(beg, end)).get_to(imageFileInfoList);
+			LOG_INFO("Assembly readed.");
+		}
+	}
+
+	inline void ImageProxy::saveData()
+	{
+		std::thread{
+			[=] {
+					nlohmann::json j = imageFileInfoList;
+					auto storageData{ j.dump() };
+					std::ofstream ofs(getAssemblyPath());
+					ofs.write(storageData.data(), storageData.size());
+					ofs.close();
+					LOG_INFO("Assembly saved.");
+				}
+		}.detach();
+	}
+
+	inline void ImageProxy::syncWithFile()
+	{
+		bool ifUpdate = false;
+		for (auto it = imageFileInfoList.begin(); it != imageFileInfoList.end();)
+		{
+			if (!file::access(mergeImageStorage(it->getStorage())))
+			{
+				it = imageFileInfoList.erase(it);
+				ifUpdate = true;
+			}
+			else
+				it++;
+		}
+		if (ifUpdate)
+			saveData();
+	}
+
+	inline std::string ImageProxy::viewImageHtmlPage(const ImageFileInfo& info) const
+	{
+		return viewImageHtmlPage(info.getUrl());
+	}
+
+	inline ImageFileInfo ImageProxy::createImageFile() const
+	{
+		ImageFileInfo info;
+		info.setUID(uuid::generateUUID());
+		return info;
+	}
+
+	inline const ImageFileInfo* ImageProxy::storageImage(ImageFileInfo&& info, std::string&& content)
+	{
+		// storage
+		info.completeInfo();
+
+		// save file thread
+		std::thread{ [=](std::string storage, std::string&& content)
+			{
+				std::ofstream ofs(mergeImageStorage(storage), std::ios::binary);
+				ofs.write(content.data(), content.size());
+				ofs.close();
+				LOG_INFO(storage, "Saved[", content.size(), "Bytes].");
+			}, info.getStorage(), std::forward<std::string>(content) }.detach();
+
+			LOG_INFO(info.getUID(), "["+info.getName()+"]uploaded.", info.getWidth(), "x", info.getHeight(), "author:", info.getAuthor(), "tags:", nlohmann::json(info.getTags()).dump());
+			imageFileInfoList.push_back(std::forward<ImageFileInfo>(info));
+
+		// save data
+		saveData();
+
+		return &imageFileInfoList.back();
+	}
+
+	inline void ImageProxy::removeImage(std::vector<const ImageFileInfo*> infos)
+	{
+		bool ifDelete = false;
+		for (auto info : infos)
+		{
+			if (info != nullptr)
+			{
+				ifDelete = true;
+				::remove(mergeImageStorage(info->getStorage()).c_str());
+			}
+		}
+		if (ifDelete)
+			syncWithFile();
+	}
+
+	inline const ImageFileInfo* ImageProxy::fetchImage(std::string fileName)
+	{
+		for (auto& info : imageFileInfoList)
+		{
+			if (info.getStorage() == fileName)
+				return &info;
+		}
+		return nullptr;
+	}
+
+	inline ImageProxy::~ImageProxy()
+	{
+		saveConfig();
+		saveData();
+	}
+
+	inline ImageProxy& ImageProxy::instance()
+	{
+		static ImageProxy _instance;
+		return _instance;
+	}
+}
+
+#endif // _IMAGE_PROXY_HPP_
---
+++ dev/homepage-api-test/homepage-api-test/main.cpp
@@ -0,0 +1,219 @@
+﻿#include "ImageProxy.hpp"
+
+int main()
+{
+	httplib::Server s;
+
+	auto splitToSet = [](std::string list)
+	{
+		std::set<std::string> tags;
+		auto vec {ohtoai::string::split(list, ",\\s*")};
+		for(auto&tag:vec)
+			if(!tag.empty())
+				tags.emplace(tag);
+		return tags;
+	};
+
+	
+	auto& proxy = ohtoai::ImageProxy::instance();
+
+	s.Get("/api/img", [&](const httplib::Request& req, httplib::Response& res)
+		{
+			LOG_INFO(req.method, ":", req.path, nlohmann::json(req.params).dump());
+			nlohmann::json j;
+
+			const auto invalidProcess = [&]
+			{
+				res.set_redirect("//thatboy.info/api/");
+			};
+
+			const auto emptyProcess = [&]
+			{
+				res.set_content(proxy.viewImageHtmlPage("//thatboy.info/404.html"), "text/html");
+				res.status = 404;
+			};
+
+			std::string op{ "get" }, type{ "page" };
+
+			// get parameters
+			if (req.has_param("op"))
+				op = req.get_param_value("op");
+			if (req.has_param("type"))
+				type = req.get_param_value("type");
+
+			
+			if (op == "get")
+			{
+				if (type == "page")
+				{
+					auto set = proxy.fetchImageSet(splitToSet(req.get_param_value("author")), splitToSet(req.get_param_value("tags")));
+					if(set.empty())
+						emptyProcess();
+					else{
+						auto info = set.at(rand() % set.size());
+						res.set_content(proxy.viewImageHtmlPage(*info), "text/html");
+					}
+				}
+				else if (type == "direct")
+				{
+					auto set = proxy.fetchImageSet(splitToSet(req.get_param_value("author")), splitToSet(req.get_param_value("tags")));	
+					if(set.empty())
+						emptyProcess();
+					else{
+						auto info = set.at(rand() % set.size());
+						res.set_redirect(info->getUrl());
+					}
+				}
+				else if (type == "dump")
+				{
+					auto set = proxy.fetchImageSet(splitToSet(req.get_param_value("author")), splitToSet(req.get_param_value("tags")));	
+					if(set.empty())
+						emptyProcess();
+					else{
+						auto info = set.at(rand() % set.size());
+						res.set_content(nlohmann::json(*info).dump(4), "application/json");
+					}
+				}
+				else if (type == "list")
+				{
+					nlohmann::json j;
+					auto set = proxy.fetchImageSet(splitToSet(req.get_param_value("author")), splitToSet(req.get_param_value("tags")));
+					j["count"] = set.size();
+
+					for (auto& info : set)
+					{
+						j["list"].push_back(*info);
+					}
+					j["error"] = "ok";
+					j["status"] = 200;
+					res.set_content(j.dump(4), "application/json");
+				}
+				else
+				{
+					invalidProcess();
+				}
+			}
+			else if(op == "sync")
+			{
+				proxy.syncWithFile();
+				
+				nlohmann::json j;
+				auto set = proxy.fetchImageSet();
+				j["count"] = set.size();
+
+				for (auto& info : set)
+				{
+					j["list"].push_back(*info);
+				}
+				j["error"] = "ok";
+				j["status"] = 200;
+				res.set_content(j.dump(4), "application/json");
+			}
+			else if(op == "delete")
+			{
+				bool ifUseFileName = req.has_param("storage");
+
+				nlohmann::json j;
+
+				if(ifUseFileName)
+				{
+					auto storage = req.get_param_value("storage");
+					proxy.removeImage({proxy.fetchImage(storage)});	
+					j["storage"] = storage;
+					j["error"] = "ok";
+				}
+				else
+				{
+					auto authors = splitToSet(req.get_param_value("author"));
+					auto tags = splitToSet(req.get_param_value("tags"));
+					if(!authors.empty()||!tags.empty())
+					{
+						proxy.removeImage(proxy.fetchImageSet(authors, tags));
+						j["error"] = "ok";
+					}
+					else
+					{
+						j["error"] = "need select author or tags";
+					}
+					j["author"] = authors;
+					j["tags"] = tags;
+				}
+
+				j["status"] = 200;
+				res.set_content(j.dump(4), "application/json");
+			}
+			else if(op == "reload")
+			{
+				proxy.loadConfig();
+				
+				nlohmann::json j;
+				auto set = proxy.fetchImageSet();
+				j["count"] = set.size();
+
+				for (auto& info : set)
+				{
+					j["list"].push_back(*info);
+				}
+				j["error"] = "ok";
+				j["status"] = 200;
+				res.set_content(j.dump(4), "application/json");
+			}
+			else if(op == "upload")
+			{
+				res.set_redirect("//thatboy.info/upload/");
+			}
+			else
+			{
+				invalidProcess();
+			}
+		});
+
+	s.Post("/api/img", [&](const httplib::Request& req, httplib::Response& res)
+		{
+			LOG_INFO(req.method, ":", req.path, nlohmann::json(req.params).dump());
+			nlohmann::json j;
+			if (req.has_param("op") && req.get_param_value("op") == "upload")
+			{
+				auto authorIncome = req.get_file_value("author").content;
+				auto author = authorIncome.empty() ? "undefined" : authorIncome;
+
+				auto tags = splitToSet(req.get_file_value("tags").content);
+				
+				auto info{ std::move(proxy.createImageFile()) };
+				info.setAuthor(author);
+				info.setTags(tags);
+				
+				info.setWidth(ohtoai::string::stringTo<int>(req.get_file_value("width").content));
+				info.setHeight(ohtoai::string::stringTo<int>(req.get_file_value("height").content));
+				info.setType(req.get_file_value("type").content);
+
+				const auto& fileFormData = req.get_file_value("file");
+				info.setName(fileFormData.filename);
+				info.setSize(fileFormData.content.size());
+					
+				j["img"] = *proxy.storageImage(std::move(info), std::move(*const_cast<std::string*>(&fileFormData.content)));
+								
+				j["error"] = "ok";
+				j["status"] = 200;
+			}
+			else
+			{
+				j["error"] = "unknow command";
+				j["status"] = 404;
+			}
+			res.set_content(j.dump(4), "application/json");
+		});
+
+	s.Get("/shutdown", [&](const auto&, auto&)
+		{
+			s.stop();
+		});
+
+	s.set_mount_point("/", ".");
+#if defined WIN32 || defined _WIN32
+	s.set_mount_point("/img", ohtoai::ImageProxy::instance().getFileStorageBase().c_str());
+	s.set_mount_point("/", "../../../");
+#endif
+
+	return s.listen("0.0.0.0", 80);
+}
