1523695e70c1d0a1967e04784887a3d09968bebf
OhtoAi <zhuguangfeng.thatboy@gmail.com>
2021-09-27 12:37:06 +0800

    mod kill command

--- dev/homepage-api-test/build.sh
+++ dev/homepage-api-test/build.sh
@@ -1,6 +1,6 @@
 g++ homepage-api-test/main.cpp -std=c++17 -lpthread -o ../../plugin/bin/image_proxy &&\
 echo compile complete.&&\
-(ps -ef | grep image_proxy | grep -v grep | cut -c 10-16 | xargs kill -9
+(ps -ef | grep image_proxy | grep -v grep | cut -c 10-16 | xargs kill -2
 echo kill image_proxy.&&\
 (cd ../../plugin/bin/ && br image_proxy)&&\
 echo  run image_proxy.)
