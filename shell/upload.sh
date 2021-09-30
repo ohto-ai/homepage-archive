89c2f6ea0651769d32c20a36aa747335e1819f85
OhtoAi <zhuguangfeng.thatboy@gmail.com>
2021-09-30 14:52:35 +0800

    mod upload sh

--- shell/upload.sh
+++ shell/upload.sh
@@ -3,9 +3,9 @@ if [ $# -eq 0 ];then
   echo "Usage: $0 <image_path_1> [img_path_2...]"
   exit 0
 fi
-for i in $@
+for ((i=2; i<=$#;i++))
 do
     echo -e "https:\c"
-    curl -F "image=@$1" 'http://ohtoai.top/api/img?op=upload&ret=text'
+    curl -F "image=@${!i}" 'http://ohtoai.top/api/img?op=upload&ret=text&author='${1}
     echo -e ""
 done
