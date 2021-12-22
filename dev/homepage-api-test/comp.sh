db1017b05ed957845433c6f4ab0feeb35f0e883d
OhtoAi <zhuguangfeng.thatboy@gmail.com>
2021-12-22 16:10:39 +0800

    [remove component] remove api img
    Signed-off-by: OhtoAi <zhuguangfeng.thatboy@gmail.com>

--- dev/homepage-api-test/comp.sh
+++
@@ -1,1 +0,0 @@
-g++ homepage-api-test/main.cpp -std=c++17 -lpthread -o ../../plugin/bin/image_proxy
--- dev/homepage-api-test/local_compile.sh
+++
@@ -1,1 +0,0 @@
-g++ homepage-api-test/main.cpp -std=c++17 -lpthread -DOHTOAI_LOCAL_TEST -o ./homepage-api-test/image_proxy
